48d03de7f4743ec0f6164b801c811d10c5899185
JosephO <jo101014wizcon@gmail.com>
2022-06-17 22:37:52 +0700

    Setting up cucumber profile

--- bdd/dist/reporter/cucumber-report.js
+++ bdd/dist/reporter/cucumber-report.js
@@ -1,15 +1,25 @@
-// import dotenv from 'dotenv'
-// import reporter, { Options } from 'cucumber-html-reporter'
-// import { env } from '../env/parseEnv'
-// dotenv.config({ path: env('COMMON_CONFIG_FILE') })
-// const options: Options = {
-//     theme: 'bootstrap',
-//     jsonFile: env('JSON_REPORT-FILE'),
-//     output: env('HTML_REPORT_FILE'),
-//     screenshotsDirectory: env('SCREENSHOT_PATH'),
-//     storeScreenshots: true,
-//     reportSuiteAsScenarios: true,
-//     launchReport: false,
-// }
-// reporter.generate(options)
 "use strict";
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+var _cucumberHtmlReporter = _interopRequireDefault(require("cucumber-html-reporter"));
+
+var _parseEnv = require("../env/parseEnv");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_dotenv["default"].config({
+  path: (0, _parseEnv.env)('COMMON_CONFIG_FILE')
+});
+
+var options = {
+  theme: 'bootstrap',
+  jsonFile: (0, _parseEnv.env)('JSON_REPORT-FILE'),
+  output: (0, _parseEnv.env)('HTML_REPORT_FILE'),
+  screenshotsDirectory: (0, _parseEnv.env)('SCREENSHOT_PATH'),
+  storeScreenshots: true,
+  reportSuiteAsScenarios: true,
+  launchReport: false
+};
+
+_cucumberHtmlReporter["default"].generate(options);
--- bdd/src/reporter/cucumber-report.ts
+++ bdd/src/reporter/cucumber-report.ts
@@ -1,17 +1,17 @@
-// import dotenv from 'dotenv'
-// import reporter, { Options } from 'cucumber-html-reporter'
-// import { env } from '../env/parseEnv'
+import dotenv from 'dotenv'
+import reporter, { Options } from 'cucumber-html-reporter'
+import { env } from '../env/parseEnv'
 
-// dotenv.config({ path: env('COMMON_CONFIG_FILE') })
+dotenv.config({ path: env('COMMON_CONFIG_FILE') })
 
-// const options: Options = {
-//     theme: 'bootstrap',
-//     jsonFile: env('JSON_REPORT-FILE'),
-//     output: env('HTML_REPORT_FILE'),
-//     screenshotsDirectory: env('SCREENSHOT_PATH'),
-//     storeScreenshots: true,
-//     reportSuiteAsScenarios: true,
-//     launchReport: false,
-// }
+const options: Options = {
+    theme: 'bootstrap',
+    jsonFile: env('JSON_REPORT-FILE'),
+    output: env('HTML_REPORT_FILE'),
+    screenshotsDirectory: env('SCREENSHOT_PATH'),
+    storeScreenshots: true,
+    reportSuiteAsScenarios: true,
+    launchReport: false,
+}
 
-// reporter.generate(options)
+reporter.generate(options)
